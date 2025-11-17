98fda45de24af9b0d8ce511706bdd215765f9d19
Victor Tulceanu <victor.tulceanu@gmail.com>
2025-11-16 21:59:26 -0500

    set debug to False

--- dashboard.py
+++ dashboard.py
@@ -342,5 +342,5 @@ def update_graph(ct_value, payback_value, elec_value, interest_value):
       return fig
 
 if __name__ == '__main__':
-      app.run_server(debug=True)
+      app.run_server(debug=False)
 
